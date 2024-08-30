22909b5b755f5a3c105d14b1cd96c05fee237ead
zoe <zoelisejohnston@gmail.com>
2024-08-29 18:14:52 -0700

    added lighting and rotation

--- source/sim.cpp
+++ source/sim.cpp
@@ -9,21 +9,43 @@
  *  Zoe Johnston, 2024
  */
 
+float x_angle = 0.0;
+float y_angle = 0.0;
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 const char* vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "layout (location = 1) in vec3 aNormal;\n"
+    "out vec3 FragPos;\n"
     "out vec3 Normal;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+    "   FragPos = vec3(model * vec4(aPos, 1.0));\n"
     "   Normal = aNormal;\n"
     "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
+    "in vec3 FragPos;\n"
+    "in vec3 Normal;\n"
     "out vec4 FragColor;\n"
+    "uniform vec3 lightPos;\n"
     "void main()\n"
     "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "    vec3 norm = normalize(Normal);\n"
+    "    vec3 lightDir = normalize(lightPos - FragPos);\n"
+    "    float diff = max(dot(norm, lightDir), 0.0);\n"
+    "    vec3 ambient = vec3(0.2f, 0.0f, 0.2f);\n"
+    "    vec3 diffuse = diff * vec3(1.0f, 1.0f, 0.8f);\n"
+    "    vec3 result = (ambient + diffuse) * vec3(1.0f, 0.5f, 0.7f);\n"
+    "    FragColor = vec4(result, 1.0);\n"
     "}\0";
 
 /*  Takes a GLFWwindow as its first argument and two integers indicating the new window dimensions
@@ -37,6 +59,14 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        x_angle += 0.5;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        x_angle -= 0.5;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        y_angle += 0.5;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        y_angle -= 0.5;
 }
 
 void createAndLinkProgram(unsigned int *program) {
@@ -106,7 +136,7 @@ int main() {
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     unsigned int shaderProgram;
@@ -114,10 +144,10 @@ int main() {
 
     // Vertex data
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+         0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f  // top left 
     };
     unsigned int indices[] = { 
         0, 1,  // first line
@@ -137,8 +167,10 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -148,17 +180,32 @@ int main() {
         processInput(window);
 
         // rendering commands
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.0f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
         glLineWidth(5.0);
 
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+        unsigned int ptr;
+        ptr = glGetUniformLocation(shaderProgram, "lightPos");
+        glUniform3fv(ptr, 1, glm::value_ptr(lightPos));
 
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
+        ptr = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(ptr, 1, GL_FALSE, glm::value_ptr(projection));
+        ptr = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(ptr, 1, GL_FALSE, glm::value_ptr(view));
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(x_angle), glm::vec3(1.0, 0.0, 0.0));
+        model = glm::rotate(model, glm::radians(y_angle), glm::vec3(0.0, 1.0, 0.0));
+
+        ptr = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(ptr, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
         glDrawElements(GL_LINES, 4, GL_UNSIGNED_INT, 0);
